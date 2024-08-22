9422fda22b1a99965a9e7d993e7f3c24f443609e
Aditya Lesmana <a.lesmana1711@gmail.com>
2024-08-23 00:41:32 +0700

    [FEAT] Defining the generator

--- dcgan.py
+++ dcgan.py
@@ -11,6 +11,36 @@ import torchvision.transforms as transform
 
 from torch.autograd import Variable
 
+# Defining the generator
+class G(nn.Module):
+    def __init__(self):
+        super(G, self).__init__()
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(100, 512, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(512, 256, 4, 1, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 128, 4, 1, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, 4, 1, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+
+# Defining the weights_init function that takes as input a neural network m and that will initialize all its weights
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
 # Setting some hyperparameters
 batchSize = 64
 imageSize = 64
@@ -23,11 +53,3 @@ transform = transform.Compose([transform.Resize(imageSize), transform.ToTensor()
 dataset = dset.CIFAR10(root='./data', download=True, transform=transform)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batchSize, shuffle=True, num_workers=2)
 
-# Defining the weights_init function that takes as input a neural network m and that will initialize all its weights
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(0)
